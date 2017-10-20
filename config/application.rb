eb52152da40391f9b2463ea6ee3d40c0ff07e40b
Marc Alan Phillips <mphillips@instructure.com>
2017-10-19 22:26:39 -0600

    Fix to the application.rb logic

--- config/application.rb
+++ config/application.rb
@@ -33,12 +33,12 @@ module Budgetr
           methods: %i[get post put patch delete options]
       end
     end
-  end
 
-  config.before_configuration do
-    env_file = File.join(Rails.root, 'config', 'application.yml')
-    YAML.load(File.open(env_file)).each do |key, value|
-      ENV[key.to_s] = value
-    end if File.exists?(env_file)
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'application.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end

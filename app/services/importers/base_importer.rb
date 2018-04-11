371a994dcfbb6bd07e9239e7993df1df64e0e9fa
Marc Phillips <mphillips@mphillips-l2.corp.instructure.com>
2018-04-11 16:17:35 -0600

    Fixed additional Dedupe import errors

--- app/services/importers/base_importer.rb
+++ app/services/importers/base_importer.rb
@@ -15,11 +15,13 @@ module Importers
     def import_data(data)
       accounts_and_categories_and_events
       # fail all create if error with creation
+      events = dedupe_data(data).map do |row|
+        record = create_record_hash(row)
+        create_event_and_associations(record, row['Account Name'], row['Category'])
+      end.compact
+
       ActiveRecord::Base.transaction do
-        data.map do |row|
-          record = create_record_hash(row)
-          create_event_and_associations(record, row['Account Name'], row['Category'])
-        end
+        events.each(&:save!)
       end
     end
 
@@ -27,7 +29,8 @@ module Importers
 
     def dedupe_data(data)
       data.each_with_object({}) do |datum, hash|
-        hash[datum.hash] = datum
+        datum_hash = datum.to_h
+        hash[datum_hash.hash] = datum_hash
       end.values
     end
 
@@ -44,7 +47,11 @@ module Importers
     def accounts_and_categories_and_events
       AccountPolicy::Scope.new(user, Account).resolve.map { |account| @accounts_hash[account.name] = account }
       CategoryPolicy::Scope.new(user, Category).resolve.map { |category| @categories_hash[category.title] = category }
-      EventPolicy::Scope.new(user, Event).resolve.includes(:accounts, :categories).map do |event|
+      load_events
+    end
+
+    def load_events
+      EventPolicy::Scope.new(user, Event).resolve.eager_load(:accounts, :categories).map do |event|
         @events_hash[event_hash_key(event)] = event
       end
     end
@@ -55,7 +62,7 @@ module Importers
     end
 
     def record_hash_key(record)
-      "#{record[:date].year}#{record[:date].month}#{record[:date].day}#{record[:amount]}" \
+      "#{record[:date].year}#{record[:date].month}#{record[:date].day}#{record[:amount].to_f}" \
         "#{record[:is_debit]}#{record[:description].slice(0, 5)}"
     end
 
@@ -73,7 +80,7 @@ module Importers
       if accounts_hash.include?(name)
         accounts_hash[name]
       else
-        account = Account.create!(name: name, user: user)
+        account = Account.new(name: name, user: user)
         accounts_hash[name] = account
         account
       end
@@ -83,7 +90,7 @@ module Importers
       if categories_hash.include?(title)
         categories_hash[title]
       else
-        category = Category.create!(title: title, user: user)
+        category = Category.new(title: title, user: user)
         categories_hash[title] = category
         category
       end
@@ -91,19 +98,18 @@ module Importers
 
     def create_linked_event(record, account, category)
       return if event_exists?(record, account, category)
-      event = Event.create!(user: user, **record)
+      event = Event.new(user: user, **record)
       create_account_and_category_link(event, account, category)
     end
 
     def create_account_and_category_link(event, account, category)
-      EventsAccount.create!(event: event, account: account)
-      EventsCategory.create!(event: event, category: category)
+      event.accounts << account
+      event.categories << category
       event
     end
 
     def event_exists?(record, account, category)
-      event = events_hash[record_hash_key(record)] || Event.find_by(user: user, **record)
-
+      event = events_hash[record_hash_key(record)]
       return false if event.nil?
       return true if event.accounts.include?(account) && event.categories.include?(category)
     end

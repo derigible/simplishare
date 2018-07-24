48e802ae104e9a31ed93b3e28fd94cc431c17f28
Marc Phillips <mphillips@instructure.com>
2018-07-24 11:19:38 -0600

    Fix bug of creating more than one share per entity

--- app/controllers/v1/concerns/virtual_entity_sharing.rb
+++ app/controllers/v1/concerns/virtual_entity_sharing.rb
@@ -5,17 +5,25 @@ module V1
         ve = VirtualEntity.find params[:id]
         authorize(ve)
         share_with_users = User.where(id: share_params[:users].map { |u| u[:id] }.detect { |u_id| u_id != current_user.id })
+        already_shared_with_ves = ve.entity.shared_with_except_user(current_user).to_a
         share_with_users.each do |user|
-          shared = VirtualEntity.new(
-            metadata: { permissions: share_params[:users].find { |u| u[:id] == user.id }[:permissions]},
-            shared_on: Time.zone.now,
-            user: user,
-            entity: ve.entity
-          )
-          shared.shared_on = Time.zone.now
-          shared.save!
+          already_shared_with_ve = already_shared_with_ves.find { |u| share_with_users.find { |swu| u.user_id == swu.id } }
+          permissions = share_params[:users].find { |u| u[:id] == user.id }[:permissions]
+          if already_shared_with_ve.present?
+            ve = VirtualEntity.find_by! user_id: already_shared_with_ve.user_id, entity: ve.entity
+            ve.update! metadata: { permissions: permissions }
+          else
+            ve = VirtualEntity.new(
+              metadata: { permissions: permissions },
+              shared_on: Time.zone.now,
+              user: user,
+              entity: ve.entity
+            )
+            ve.shared_on = Time.zone.now
+            ve.save!
+          end
         end
-        ves = ve.entity.shared_with_except_user(current_user)
+        ves = ve.entity.shared_with_except_user current_user
         respond_with ves, each_serializer: SharedWithSerializer
       end
 

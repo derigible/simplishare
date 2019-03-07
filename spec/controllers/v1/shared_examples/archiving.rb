cdb8d7490095666edffa12378fbf8a4ef77b7765
Marc Phillips <mphillips@instructure.com>
2019-03-07 15:47:17 -0700

    Add more tests for archive shared_examples

--- spec/controllers/v1/shared_examples/archiving.rb
+++ spec/controllers/v1/shared_examples/archiving.rb
@@ -3,7 +3,20 @@
 require 'spec_helper'
 
 shared_examples_for 'an archivable entity' do
+  let(:overrides) { {} }
+  let(:ve) { factory.entity overrides: { virtual_entity: { user: user }.merge(overrides) } }
+  let(:json_schema) { raise 'Override in spec' }
+  let(:factory) { raise 'Override in spec' }
+
   describe 'index' do
+    subject { get :index, params: params }
+
+    let(:params) { {} }
+
+    before do
+      3.times.each { factory.entity overrides: { virtual_entity: { user: user } } }
+    end
+
     context 'with archived entity' do
       it 'does not send archived entities'
 
@@ -24,6 +37,11 @@ shared_examples_for 'an archivable entity' do
   end
 
   describe 'update' do
+    subject { put :archive, params: params.merge(update_shared), as: :json }
+
+    let(:params) { { id: ve.id } }
+    let(:update_shared) { {} }
+
     context 'when only user' do
       it 'archives virtual_entity only'
 
@@ -31,7 +49,7 @@ shared_examples_for 'an archivable entity' do
     end
 
     context 'when shared' do
-      context 'when not owner or archive privileges denied' do
+      context 'when not owner and archive privileges denied' do
         it { is_expected.to have_http_status(:forbidden) }
 
         it 'does not send email'
@@ -43,16 +61,23 @@ shared_examples_for 'an archivable entity' do
         it 'archives the virtual_entity'
 
         context 'when update_shared is true' do
+          let(:update_shared) { { update_shared: true } }
+
           it 'archives the entity'
 
-          it ''
+          it 'removes from all users index'
+
+          it 'sends an email to all users'
         end
       end
 
       context 'when owner' do
-
+        it_behaves_like 'can_archive'
       end
-      context 'when archive privileges granted'
+
+      context 'when archive privileges granted' do
+        it_behaves_like 'can_archive'
+      end
     end
   end
 end

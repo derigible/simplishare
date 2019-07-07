7f356ac1ed0052ef8c1556d07db06e8518902888
Marc Phillips <mphillips@instructure.com>
2019-07-06 18:50:44 -0600

    Make Contacts Page

--- ui/src/resources/User/Contacts/index.js
+++ ui/src/resources/User/Contacts/index.js
@@ -2,70 +2,87 @@
 
 import React from 'react'
 
+import { Button } from '@instructure/ui-buttons'
+import { Table } from '@instructure/ui-table'
 import { Grid } from '@instructure/ui-layout'
 import { View } from '@instructure/ui-layout'
 import { Heading } from '@instructure/ui-elements'
 import { Text } from '@instructure/ui-elements'
 
+import { IconPlusLine } from '@instructure/ui-icons'
+
 import type { Contact as ContactType } from '../type'
 
-function Contact ({contact} : {contact: ContactType}) {
+function ContactTable ({type, contacts} : {type: string, contacts: Array<ContactType>}) {
   return (
-    <View as="div" margin="small none">
-      <Grid>
-        <Grid.Row>
-          <Grid.Col>
-            <View
-              as="div"
-              display="inline-block"
-              margin="none small none none"
-              padding="small"
-              background="inverse"
-            >
-              Username
-            </View>
-            <Text size="large">{contact.username || 'pending'}</Text>
-          </Grid.Col>
-          <Grid.Col>
-            <View
-              as="div"
-              display="inline-block"
-              margin="none small none none"
-              padding="small"
-              background="inverse"
-            >
-              Email
-            </View>
-            <Text size="large">{contact.email}</Text>
-          </Grid.Col>
-        </Grid.Row>
-      </Grid>
-    </View>
+    <>
+      <Heading>{type}</Heading>
+      <Table
+        caption={`${type} Contacts`}
+        hover
+      >
+        <Table.Head>
+          <Table.Row>
+            <Table.RowHeader>Username</Table.RowHeader>
+            <Table.RowHeader>Email</Table.RowHeader>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {
+            contacts.map(c => (
+              <Table.Row key={c.email}>
+                <Table.Cell>{c.username || 'Pending'}</Table.Cell>
+                <Table.Cell>{c.email}</Table.Cell>
+              </Table.Row>
+            ))
+          }
+        </Table.Body>
+      </Table>
+    </>
   )
 }
 
 export default function Contacts ({contacts} : {contacts: Array<ContactType>}) {
-  const pending = contacts.filter(c => !c.username)
-  const not_pending = contacts.filter(c => c.username)
-
-  return (
-    <Grid>
-      <Grid.Row>
-        <Grid.Col>
-          <Heading>Connected</Heading>
-          {
-            not_pending.map(c => (<Contact key={c.contact_id} contact={c} />))
-          }
-        </Grid.Col>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Col>
-          <Heading>Pending</Heading>
-          {
-            pending.map(c => (<Contact key={c.contact_id} contact={c} />))
-          }
-        </Grid.Col>
-      </Grid.Row>
-    </Grid>
+  const addButton = () => (
+    <View as="div" textAlign="end">
+      <Button variant="primary" icon={IconPlusLine}  margin="small">
+        Add Contact
+      </Button>
+    </View>
   )
+
+  if (contacts.length > 0) {
+    const pending = contacts.filter(c => !c.username)
+    const not_pending = contacts.filter(c => c.username)
+
+    return (
+      <>
+        {addButton()}
+        <Grid>
+          <Grid.Row>
+            <Grid.Col>
+              <ContactTable type="Connected" contacts={not_pending} />
+            </Grid.Col>
+          </Grid.Row>
+          {pending.length > 0
+            ? <Grid.Row>
+                <Grid.Col>
+                  <ContactTable type="Pending" contacts={pending} />
+                </Grid.Col>
+              </Grid.Row>
+            : null
+          }
+        </Grid>
+      </>
+    )
+  } else {
+    return (
+      <>
+        {addButton()}
+        <View>
+          No Contacts!
+        </View>
+      </>
+    )
+  }
 }

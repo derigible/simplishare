9c417a103ad78d503be01f5c98135f7eb5a072ab
Marc Phillips <mphillips@instructure.com>
2019-07-10 09:53:58 -0600

    Add pages to share modal

--- ui/src/components/StandardActions/Share/index.js
+++ ui/src/components/StandardActions/Share/index.js
@@ -4,13 +4,14 @@ import React from 'react'
 
 import { View } from '@instructure/ui-layout'
 import { Button } from '@instructure/ui-buttons'
-import { IconShareLine } from '@instructure/ui-icons'
+import { IconShareLine, IconAddLine } from '@instructure/ui-icons'
 import { Tooltip } from '@instructure/ui-overlays'
 import { ScreenReaderContent } from '@instructure/ui-a11y'
 import { Table } from '@instructure/ui-table'
 import { Pill } from '@instructure/ui-elements'
 import { Popover } from '@instructure/ui-overlays'
 import { CheckboxGroup, Checkbox } from '@instructure/ui-forms'
+import { Pages } from '@instructure/ui-pages'
 
 import StandardEditModal from '../../StandardEditModal'
 import ClickableDiv from '../../ClickableDiv'
@@ -64,10 +65,64 @@ function renderAccess(contact: SharedWithContact) {
   )
 }
 
+function PageOne ({entity, nextPage}, {entity: VirtualEntity, nextPage: any}) {
+  return (
+    <>
+      <Table
+        caption="Manage Shared Settings"
+        hover
+      >
+        <Table.Head>
+          <Table.Row>
+            <Table.RowHeader>Username</Table.RowHeader>
+            <Table.RowHeader>Email</Table.RowHeader>
+            <Table.RowHeader>Access</Table.RowHeader>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {
+            entity.sharedWith.map(sw => (
+              <Table.Row key={sw.id}>
+                <Table.Cell>{sw.username || 'Pending'}</Table.Cell>
+                <Table.Cell>{sw.email}</Table.Cell>
+                <Table.Cell>{renderAccess(sw)}</Table.Cell>
+              </Table.Row>
+            ))
+          }
+        </Table.Body>
+      </Table>
+      <Button icon={IconAddLine} variant="primary" onClick={nextPage} margin="small none">Add User</Button>
+    </>
+  )
+}
+
+function PageTwo ({entity, back}, {entity: VirtualEntity, back: any}) {
+  const [userId: ?string, setUserId: any] = React.useState(null)
+  const [perms: Array<string>, setPerms: any] = React.useState([])
+
+  return (
+    <>
+      hello
+      <View as="div" textAlign="end">
+        <Button onClick={back} margin="small">Cancel</Button>
+        <Button
+          disabled={!!userId}
+          onClick={() => {entity.shareWith(userId, perms); setUserId(null); setPerms([]); back()}}
+          variant="primary"
+          margin="small none"
+        >
+          Share
+        </Button>
+      </View>
+    </>
+  )
+}
+
 export default function Share(
   {entity} : {entity: VirtualEntity}
 ) {
   const [modalOpen: boolean, setModalOpen] = React.useState(false)
+  const [page: number, setPage] = React.useState(0)
 
   const toggleModal = () => setModalOpen(!modalOpen)
 
@@ -78,29 +133,18 @@ export default function Share(
         modalOpen={modalOpen}
         modalTitle={`Share ${entity.displayName}`}
       >
-        <Table
-          caption="Manage Shared Settings"
-          hover
+        <Pages
+          activePageIndex={page}
+          onPageIndexChange={setPage}
+          backButtonLabel="Back to shared settings"
         >
-          <Table.Head>
-            <Table.Row>
-              <Table.RowHeader>Username</Table.RowHeader>
-              <Table.RowHeader>Email</Table.RowHeader>
-              <Table.RowHeader>Access</Table.RowHeader>
-            </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            {
-              entity.sharedWith.map(sw => (
-                <Table.Row key={sw.id}>
-                  <Table.Cell>{sw.username || 'Pending'}</Table.Cell>
-                  <Table.Cell>{sw.email}</Table.Cell>
-                  <Table.Cell>{renderAccess(sw)}</Table.Cell>
-                </Table.Row>
-              ))
-            }
-          </Table.Body>
-        </Table>
+          <Pages.Page>
+            {(_, _2) => (<PageOne entity={entity} nextPage={() => setPage(1)} />)}
+          </Pages.Page>
+          <Pages.Page>
+            {(_, navigateToPreviousPage) => (<PageTwo entity={entity} back={navigateToPreviousPage} />)}
+          </Pages.Page>
+        </Pages>
       </StandardEditModal>
       <Tooltip
         tip={`Share Settings for ${entity.displayName}`}
